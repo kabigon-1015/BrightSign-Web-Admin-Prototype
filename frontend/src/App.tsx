539ebf09ad1996a51d24de7d591af3a37178e83a
kabigon-1015 <sakabe.kentaro@mikilab.doshisha.ac.jp>
2024-02-16 01:18:22 +0900

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="navbar bg-base-300">
+        <button className="btn btn-ghost text-xl">画像や動画の管理</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
